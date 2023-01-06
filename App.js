f24571231bfbe644404f09e0fb0d1ee6c526a83d
tomyrodeghiero <tomyrodeghiero@gmail.com>
2023-01-06 15:44:57 -0300

    ChatApp Repository

---
+++ App.js
@@ -0,0 +1,27 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Chat from "./screens/Chat";
+import Login from "./screens/Login";
+
+const Stack = createStackNavigator();
+
+function ChatStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+    </Stack.Navigator>
+  );
+}
+
+function RootNavigator() {
+  return (
+    <NavigationContainer>
+      <ChatStack />
+    </NavigationContainer>
+  );
+}
+
+export default function App() {
+  return <RootNavigator />;
+}
